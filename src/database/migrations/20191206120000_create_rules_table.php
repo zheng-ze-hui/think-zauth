c5f6362e472e20f6dc60cd2365e56a1e88d31a9b
Zheng Hui <2366690@qq.com>
2019-12-06 12:29:46 +0800

    重新试试数据迁移

--- src/database/migrations/20191206120000_create_rules_table.php
+++ src/database/migrations/20191206120000_create_rules_table.php
@@ -1,108 +1,22 @@
 <?php
 
 use think\migration\Migrator;
-use Phinx\Db\Adapter\AdapterFactory;
+use think\migration\db\Column;
 
 class CreateRulesTable extends Migrator
 {
-    /**
-     * Initialize method.
-     *
-     * @return void
-     */
-    protected function init()
+    public  function  change()
     {
-        $options = $this->getDbConfig();
-    
-        $adapter = AdapterFactory::instance()->getAdapter($options['adapter'], $options);
-    
-        if ($adapter->hasOption('table_prefix') || $adapter->hasOption('table_suffix')) {
-            $adapter = AdapterFactory::instance()->getWrapper('prefix', $adapter);
-        }
-    
-        $this->setAdapter( $adapter);    
-    }
-    
-    /**
-     * 获取数据库配置
-     * @return array
-     */
-    protected function getDbConfig(): array
-    {
-        $default = config('database.default');
-
-        $config = config("database.connections.{$default}");
-
-        if (0 == $config['deploy']) {
-            $dbConfig = [
-                'adapter'      => $config['type'],
-                'host'         => $config['hostname'],
-                'name'         => $config['database'],
-                'user'         => $config['username'],
-                'pass'         => $config['password'],
-                'port'         => $config['hostport'],
-                'charset'      => $config['charset'],
-                'table_prefix' => $config['prefix'],
-            ];
-        } else {
-            $dbConfig = [
-                'adapter'      => explode(',', $config['type'])[0],
-                'host'         => explode(',', $config['hostname'])[0],
-                'name'         => explode(',', $config['database'])[0],
-                'user'         => explode(',', $config['username'])[0],
-                'pass'         => explode(',', $config['password'])[0],
-                'port'         => explode(',', $config['hostport'])[0],
-                'charset'      => explode(',', $config['charset'])[0],
-                'table_prefix' => explode(',', $config['prefix'])[0],
-            ];
-        }
-
-        $table = config('database.migration_table', 'migrations');
-
-        $dbConfig['default_migration_table'] = $dbConfig['table_prefix'] . $table;
-
-        return $dbConfig;
-    }
-
-    /**
-     * Change Method.
-     *
-     * Write your reversible migrations using this method.
-     *
-     * More information on writing migrations is available here:
-     * http://docs.phinx.org/en/latest/migrations.html#the-abstractmigration-class
-     *
-     * The following commands can be used in this method and Phinx will
-     * automatically reverse them when rolling back:
-     *
-     *    createTable
-     *    renameTable
-     *    addColumn
-     *    renameColumn
-     *    addIndex
-     *    addForeignKey
-     *
-     * Remember to call "create()" or "update()" and NOT "save()" when working
-     * with the Table class.
-     */
-    public function up()
-    {
-        // $default = config('tauthz.default');
-        $table = $this->table('prefix_rules');
-        $table->addColumn('ptype', 'string', ['null' => true])
-            ->addColumn('v0', 'string', ['null' => true])
-            ->addColumn('v1', 'string', ['null' => true])
-            ->addColumn('v2', 'string', ['null' => true])
-            ->addColumn('v3', 'string', ['null' => true])
-            ->addColumn('v4', 'string', ['null' => true])
-            ->addColumn('v5', 'string', ['null' => true])
-            ->create();
-    }
-
-    public function down()
-    {
-        // $default = config('tauthz.default');
-        // $table = $this->table(config('tauthz.enforcers.'.$default.'.database.rules_name'));
-        // $table->drop();
+        // create the table
+        $table  =  $this->table('users',array('engine'=>'MyISAM'));
+        $table->addColumn('username', 'string',array('limit'  =>  15,'default'=>'','comment'=>'用户名，登陆使用'))
+        ->addColumn('password', 'string',array('limit'  =>  32,'default'=>md5('123456'),'comment'=>'用户密码')) 
+        ->addColumn('login_status', 'boolean',array('limit'  =>  1,'default'=>0,'comment'=>'登陆状态'))
+        ->addColumn('login_code', 'string',array('limit'  =>  32,'default'=>0,'comment'=>'排他性登陆标识'))
+        ->addColumn('last_login_ip', 'integer',array('limit'  =>  11,'default'=>0,'comment'=>'最后登录IP'))
+        ->addColumn('last_login_time', 'datetime',array('default'=>0,'comment'=>'最后登录时间'))
+        ->addColumn('is_delete', 'boolean',array('limit'  =>  1,'default'=>0,'comment'=>'删除状态，1已删除'))
+        ->addIndex(array('username'), array('unique'  =>  true))
+        ->create();
     }
 }
