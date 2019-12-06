a18bd199a31c517479a2f83a47370af79ed2fe20
Zheng Hui <2366690@qq.com>
2019-12-06 17:06:07 +0800

    定义数据表结构

--- src/database/migrations/20191206120000_create_zauth_rules_table.php
+++ src/database/migrations/20191206120000_create_zauth_rules_table.php
@@ -9,10 +9,10 @@ class CreateZauthRulesTable extends Migrator
     {
         // create the table
         $table  =  $this->table('zauth_rules',array('collation'=>'utf8mb4_unicode_ci', 'comment'=>'规则表'));
-        $table->addColumn('url', 'string',array('limit' => 80,'default'=>'','comment'=>'规则url'))
-        ->addColumn('title', 'string',array('limit' => 20,'default'=>'','comment'=>'规则标题')) 
+        $table->addColumn('url', 'string',array('limit' => 80,'default'=>NULL,'null'=>true,'comment'=>'规则url'))
+        ->addColumn('title', 'string',array('limit' => 20,'default'=>NULL,'comment'=>'规则标题')) 
         ->addColumn('type', 'integer',array('limit' => 1,'default'=>1,'comment'=>'规则类型,1导航,2其他如按钮等'))
-        ->addColumn('icon', 'string',array('limit' => 32,'default'=>'','comment'=>'图标'))
+        ->addColumn('icon', 'string',array('limit' => 32,'default'=>NULL,'comment'=>'图标'))
         ->addColumn('rule_order', 'integer',array('limit' => 3,'default'=>0,'comment'=>'规则排序'))
         ->addColumn('status', 'integer',array('limit' => 1,'default'=>1,'comment'=>'状态,0禁用,1启用'))
         ->addIndex(array('url', 'title'), array('unique'  =>  true))
