1032f993656700f0403e9217e62eb84c66e2cd08
Zheng Hui <2366690@qq.com>
2019-12-06 11:27:31 +0800

    注册数据迁移服务

--- src/ZauthService.php
+++ src/ZauthService.php
@@ -6,6 +6,22 @@ use think\Service;
 
 class ZauthService extends Service
 {
+    /**
+     * 注册服务.
+     *
+     * @return void
+    **/
+    public function register()
+    {
+        // 注册数据迁移服务
+        $this->app->register(\think\migration\Service::class);
+    }
+
+    /**
+     * 启动函数
+     *
+     * @return void
+    **/
     public function boot()
     {
 
