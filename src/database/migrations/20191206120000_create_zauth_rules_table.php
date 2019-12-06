4b4b2f4abf1a07de719a75377ee5a42d0cf65545
Zheng Hui <2366690@qq.com>
2019-12-06 14:13:20 +0800

    修改数据结构

--- src/database/migrations/20191206120000_create_zauth_rules_table.php
+++ src/database/migrations/20191206120000_create_zauth_rules_table.php
@@ -10,13 +10,13 @@ class CreateZauthRulesTable extends Migrator
         // create the table
         $table  =  $this->table('zauth_rules',array('collation'=>'utf8mb4_unicode_ci', 'comment'=>'规则表'));
         $table->addColumn('url', 'string',array('limit' => 80,'default'=>'','comment'=>'规则url'))
-        ->addColumn('password', 'string',array('limit'  =>  32,'default'=>md5('123456'),'comment'=>'用户密码')) 
+        ->addColumn('title', 'string',array('limit'  =>  20,'default'=>'','comment'=>'规则标题')) 
         ->addColumn('login_status', 'boolean',array('limit'  =>  1,'default'=>0,'comment'=>'登陆状态'))
         ->addColumn('login_code', 'string',array('limit'  =>  32,'default'=>0,'comment'=>'排他性登陆标识'))
         ->addColumn('last_login_ip', 'integer',array('limit'  =>  11,'default'=>0,'comment'=>'最后登录IP'))
         ->addColumn('last_login_time', 'integer',array('default'=>0,'comment'=>'最后登录时间'))
         ->addColumn('is_delete', 'boolean',array('limit'  =>  1,'default'=>0,'comment'=>'删除状态，1已删除'))
-        ->addIndex(array('username'), array('unique'  =>  true))
+        ->addIndex(array('url', 'title'), array('unique'  =>  true))
         ->create();
     }
 }
