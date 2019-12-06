3c121f05631128075bba8b929e2fb8676faa0013
Zheng Hui <2366690@qq.com>
2019-12-06 16:56:12 +0800

    定义数据结构

--- src/database/migrations/20191206120000_create_zauth_rules_table.php
+++ src/database/migrations/20191206120000_create_zauth_rules_table.php
@@ -10,12 +10,11 @@ class CreateZauthRulesTable extends Migrator
         // create the table
         $table  =  $this->table('zauth_rules',array('collation'=>'utf8mb4_unicode_ci', 'comment'=>'规则表'));
         $table->addColumn('url', 'string',array('limit' => 80,'default'=>'','comment'=>'规则url'))
-        ->addColumn('title', 'string',array('limit'  =>  20,'default'=>'','comment'=>'规则标题')) 
-        ->addColumn('login_status', 'boolean',array('limit'  =>  1,'default'=>0,'comment'=>'登陆状态'))
-        ->addColumn('login_code', 'string',array('limit'  =>  32,'default'=>0,'comment'=>'排他性登陆标识'))
-        ->addColumn('last_login_ip', 'integer',array('limit'  =>  11,'default'=>0,'comment'=>'最后登录IP'))
-        ->addColumn('last_login_time', 'integer',array('default'=>0,'comment'=>'最后登录时间'))
-        ->addColumn('is_delete', 'boolean',array('limit'  =>  1,'default'=>0,'comment'=>'删除状态，1已删除'))
+        ->addColumn('title', 'string',array('limit' => 20,'default'=>'','comment'=>'规则标题')) 
+        ->addColumn('type', 'integer',array('limit' => 1,'default'=>1,'comment'=>'规则类型,1导航,2其他如按钮等'))
+        ->addColumn('icon', 'string',array('limit' => 32,'default'=>'','comment'=>'图标'))
+        ->addColumn('rule_order', 'integer',array('limit' => 3,'default'=>0,'comment'=>'规则排序'))
+        ->addColumn('status', 'integer',array('limit' => 1,'default'=>1,'comment'=>'状态,0禁用,1启用'))
         ->addIndex(array('url', 'title'), array('unique'  =>  true))
         ->create();
     }
